fd7d8e48ae2f9557310c6ea829e122dba3b70e78
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-04-14 11:31:38 +0700

    update dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -161,7 +161,7 @@
             <div class="col-sm-4">
               <div class="card ">
                 <div class="card-body card-1">
-                  <div class="row"> 
+                  <div class="row">
                     <div class="col mt-0">
                       <div class="row">
                         <div class="col-sm-2">
@@ -188,7 +188,7 @@
             <div class="col-sm-4">
               <div class="card ">
                 <div class="card-body card-2">
-                  <div class="row"> 
+                  <div class="row">
                     <div class="col mt-0">
                       <div class="row">
                         <div class="col-sm-2">
@@ -215,7 +215,7 @@
             <div class="col-sm-4">
               <div class="card ">
                 <div class="card-body card-3">
-                  <div class="row"> 
+                  <div class="row">
                     <div class="col mt-0">
                       <div class="row">
                         <div class="col-sm-2">
@@ -238,105 +238,45 @@
 
                 </div>
               </div>
+            </div>
+
           </div>
-          <!-- <div class="col-xl-6 col-xxl-5 d-flex">
-              <div class="w-100">
-                <div class="row">
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col mt-0">
-                            <h5 class="card-title">Sales</h5>
-                          </div>
 
-                          <div class="col-auto">
-                            <div class="stat text-primary">
-                              <i class="align-middle" data-feather="truck"></i>
-                            </div>
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card ">
+                <div class="card-body card-1">
+                  <div class="row">
+                    <div class="col mt-0">
+                      <div class="row">
+                        <div class="col-sm-2">
+                          <div class="stat text-primary">
+                            <img src="/assets/image/box.png" alt="">
                           </div>
                         </div>
-                        <h1 class="mt-1 mb-3">2.382</h1>
-                        <div class="mb-0">
-                          <span class="text-danger">
-                            <i class="mdi mdi-arrow-bottom-right"></i> -3.65%
-                          </span>
-                          <span class="text-muted">Since last week</span>
+                        <div class="col-sm-10" style="justify-content: center;">
+                          <p class="card-title">Total Stock</p>
                         </div>
-                      </div>
-                    </div>
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col mt-0">
-                            <h5 class="card-title">Visitors</h5>
-                          </div>
 
-                          <div class="col-auto">
-                            <div class="stat text-primary">
-                              <i class="align-middle" data-feather="users"></i>
-                            </div>
-                          </div>
-                        </div>
-                        <h1 class="mt-1 mb-3">14.212</h1>
-                        <div class="mb-0">
-                          <span class="text-success">
-                            <i class="mdi mdi-arrow-bottom-right"></i> 5.25%
-                          </span>
-                          <span class="text-muted">Since last week</span>
-                        </div>
+
                       </div>
+
+
                     </div>
+
                   </div>
-                  <div class="col-sm-6">
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col mt-0">
-                            <h5 class="card-title">Earnings</h5>
-                          </div>
+                  <h1 class="card-count mt-1 mb-3">2.382</h1>
 
-                          <div class="col-auto">
-                            <div class="stat text-primary">
-                              <i class="align-middle" data-feather="dollar-sign"></i>
-                            </div>
-                          </div>
-                        </div>
-                        <h1 class="mt-1 mb-3">$21.300</h1>
-                        <div class="mb-0">
-                          <span class="text-success">
-                            <i class="mdi mdi-arrow-bottom-right"></i> 6.65%
-                          </span>
-                          <span class="text-muted">Since last week</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col mt-0">
-                            <h5 class="card-title">Orders</h5>
-                          </div>
-
-                          <div class="col-auto">
-                            <div class="stat text-primary">
-                              <i class="align-middle" data-feather="shopping-cart"></i>
-                            </div>
-                          </div>
-                        </div>
-                        <h1 class="mt-1 mb-3">64</h1>
-                        <div class="mb-0">
-                          <span class="text-danger">
-                            <i class="mdi mdi-arrow-bottom-right"></i> -2.25%
-                          </span>
-                          <span class="text-muted">Since last week</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
                 </div>
-                  </div>
-                </div> -->
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div>
