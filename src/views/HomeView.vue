470f2727551835786833aef2d76d4eb33d0060b5
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-06 09:54:33 +0700

    update dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -144,11 +144,9 @@ export default {
           </div>
 
           <div class="col-md-3  offcanvas-header-right">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="button-closed bi bi-list"
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="button-closed bi bi-x"
               viewBox="0 0 16 16" data-bs-dismiss="offcanvas" aria-label="Close">
-              <path fill-rule="evenodd"
-                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-            </svg>
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>            </svg>
           </div>
         </div>
       </div>
