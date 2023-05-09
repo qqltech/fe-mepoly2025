e66c7676f2bb918596c1455d1cc7f572bf0cbf31
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-09 20:02:03 +0700

    update dashboard - filter period

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -204,7 +204,7 @@ export default {
     const dd = String(today.getDate()).padStart(2, '0');
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const yyyy = today.getFullYear();
-    const formattedDate = `${dd}-${mm}-${yyyy}`;
+    const formattedDate = `${yyyy}-${mm}-${dd}`;
     this.periodeEnd = formattedDate;
     this.periodeStart = formattedDate;
   },
