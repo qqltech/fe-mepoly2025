6fbb76fcdc2c467ee9f6a2d05b72264d8cb79562
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-06-01 09:04:35 +0700

    Update - REVISIIIIIIIIII - offcanvas fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -664,7 +664,7 @@ export default {
             </div>
             <hr class="garis-sidebar" style=" margin-top: 20px" />
 
-            <div class="d-grid gap-2 mt-2">
+            <div class="d-grid gap-2 mt-2 mb-4">
               <router-link to="" @click="logout" aria-expanded="true" tag="button"
                 class="button4 btn-danger rtlink-btn4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
                   fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
@@ -673,6 +673,7 @@ export default {
                     d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z" />
                 </svg> Logout</router-link>
             </div>
+            
           </div>
         </div>
 
