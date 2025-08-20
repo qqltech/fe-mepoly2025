2f2a41d4d02aa74fb744e78b80e11466fa3f212b
Septian Fahmi Ardiansyah <fahrisepti888@gmail.com>
2025-08-20 16:54:30 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1178,13 +1178,14 @@ export default {
               </div>
             </div>
             <hr class="garis-sidebar" style="margin-top: 20px" />
-            <div class="d-grid gap-2 mt-2">
+            <div class="d-grid gap-2 mt-2" v-if="role === 'Admin'">
               <router-link
                 to="/customers"
                 aria-expanded="true"
                 tag="button"
                 class="button3 btn-primary rtlink-btn4"
-                ><svg
+              >
+                <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
@@ -1196,8 +1197,8 @@ export default {
                     d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"
                   />
                 </svg>
-                Customer List</router-link
-              >
+                Customer List
+              </router-link>
             </div>
             <div class="d-grid gap-2 mt-2">
               <router-link
